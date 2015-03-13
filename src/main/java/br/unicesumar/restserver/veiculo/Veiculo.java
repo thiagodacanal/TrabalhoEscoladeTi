017a6219ed05354b27c8eedb6cef6c863f41d016
thiagodacanal <thiagodacanal@gmail.com>
2015-03-12 22:13:33 -0300

    alteradosclasseveiculoeclasseveiculocontroller

--- src/main/java/br/unicesumar/restserver/veiculo/Veiculo.java
+++ src/main/java/br/unicesumar/restserver/veiculo/Veiculo.java
@@ -1,8 +1,88 @@
 package br.unicesumar.restserver.veiculo;
 
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 
+@Entity
 public class Veiculo {
+    
+    @Id
+    private Long id;
+    private String placa;
+    private Integer anoFabricacao;
+    private Integer anoModelo;
+
+    public Veiculo() {
+    }
+
+    public Veiculo(Long id, String placa, Integer anoFabricacao, Integer anoModelo) {
+        this.id = id;
+        this.placa = placa;
+        this.anoFabricacao = anoFabricacao;
+        this.anoModelo = anoModelo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public Integer getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public Integer getAnoModelo() {
+        return anoModelo;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setAnoFabricacao(Integer anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public void setAnoModelo(Integer anoModelo) {
+        this.anoModelo = anoModelo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Veiculo other = (Veiculo) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo{" + "id=" + id + ", placa=" + placa + ", anoFabricacao=" + anoFabricacao + ", anoModelo=" + anoModelo + '}';
+    }
+    
     
     
 }
